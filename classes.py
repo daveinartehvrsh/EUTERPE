abee5f2b2be88d6c8eb046feb4c35cb834b5aa66
daveinartehvrsh <davide.abourachid@gmail.com>
2023-03-08 20:02:51 +0100

    init dev

---
+++ classes.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+
+# ABSTRACT
+
+class Component:
+    pass
+
+    def setName(self, name):
+        pass
+
+    def 
+
+class Container:
+    pass
+
+
+# IMPLEMENETED
+
+@dataclass(slots=True)
+class Loop:
+    pass
+
+@dataclass(slots=True)
+class Loopkit:
+    pass
+
+@dataclass(slots=True)
+class Dataset:
+    pass
+
+#################################
+
+@dataclass(slots=True)
+class Trackout:
+    pass
+
+@dataclass(slots=True)
+class Stems:
+    pass
+
+@dataclass(slots=True)
+class Mix:
+    pass
+
+#################################
+
+class Brain:
+    pass
+
+class Plotter:
+    pass
+
+#################################
