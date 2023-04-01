0e767c18503890252acbdb7d2f831589ea3b0088
daveinartehvrsh <davide.abourachid@gmail.com>
2023-04-01 18:43:09 +0200

    added club preset

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from lib.calliope import Calliope
 import lib.presets as presets
 
 system_info = presets.get_system_config()
-presets.load_preset('drill', system_info)
+presets.load_preset('club', system_info)
 
 calliope = Calliope(system_info)
 
