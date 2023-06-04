a6dbbb27c9332d342765bff959f070d8b8b8dcfc
daveinartehvrsh <davide.abourachid@gmail.com>
2023-06-04 12:14:45 +0200

    upd

--- add_preset.py
+++ add_preset.py
@@ -8,10 +8,10 @@ while True:
     channel_name = input('channel name: ')
     preset[channel_name] = {}
     preset[channel_name]['path'] = easygui.diropenbox(title=f'path for {channel_name}')
-    preset[channel_name]['n_loops'] = int(input('n_loops: '))
-    preset[channel_name]['intensity'] = int(input('intensity: '))
-    preset[channel_name]['gain'] = int(input('gain: '))
-    preset[channel_name]['tunes'] = int(input('tunes: '))
+    preset[channel_name]['n_loops'] = float(input('n_loops: '))
+    preset[channel_name]['intensity'] = float(input('intensity: '))
+    preset[channel_name]['gain'] = float(input('gain: '))
+    preset[channel_name]['tunes'] = float(input('tunes: '))
     if input('add another channel? (y/n) ') == 'n':
         break
 
---
+++ run.py
@@ -0,0 +1,25 @@
+import subprocess
+import pkg_resources
+import sys
+
+
+def check_install_library(library_name):
+    try:
+        dist = pkg_resources.get_distribution(library_name)
+        print(f"{dist.key} {dist.version} is already installed.")
+    except pkg_resources.DistributionNotFound:
+        print(f"{library_name} is not installed. Installing now...")
+        subprocess.check_call(['pip', 'install', library_name])
+        print(f"{library_name} has been installed successfully.")
+
+if len(sys.argv) > 1 and sys.argv[1] == "28":
+    print("Verified identity")
+else:
+    sys.exit(1)
+
+required_libraries = ['librosa', 'numpy', 'scipy', 'pandas', 'soundfile']
+
+for library in required_libraries:
+    check_install_library(library)
+
+subprocess.check_call(['python', 'main.py'])
